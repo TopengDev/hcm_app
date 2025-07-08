90a9d0383cbadf75f9685abfa28bcd0459d4d7a2
TopengDev <topengdev@outlook.com>
2025-07-08 15:52:40 +0700

    :hammer: ~ update fields pasien

--- src/services/nurses.service.ts
+++ src/services/nurses.service.ts
@@ -7,6 +7,8 @@ export async function registerNurse(payload: FormData) {
    try {
       const newNurse: any = Object.fromEntries(payload.entries());
 
+      delete newNurse.age;
+
       const existingNurseByEmail = await db.query.nurses.findFirst({
          where: (nurses, { eq }) => eq(nurses.email, newNurse.email),
       });
